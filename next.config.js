da0137e2c6009f6e9077fe79d95768b95ec97239
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-09-30 16:38:09 +0800

    feat: 增加页面切换进度条

--- next.config.js
+++ next.config.js
@@ -8,18 +8,6 @@ const nextConfig = {
 	pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 };
 
-// const withMDX = require('@next/mdx')({
-// 	extension: /\.mdx?$/,
-// 	options: {
-// 		remarkPlugins: [],
-// 		rehypePlugins: [],
-// 		providerImportSource: '@mdx-js/react',
-// 	},
-// });
-
 module.exports = {
 	...nextConfig,
-	// ...withMDX({
-	// 	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-	// }),
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 import { Layout } from '@/components';
 
 import 'antd/dist/antd.css';
+import '@/styles/nprogress.scss';
 import '../styles/globals.scss';
 import '../styles/markdown.scss';
 import Head from 'next/head';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
